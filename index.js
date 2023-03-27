040a400a0ddffcbb001cf4b9ee4f985ec3a41fea
Githmin Jayawardhana <Githmin21@gmail.com>
2023-03-27 11:57:23 +0530

    integrated openai

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const { Client, IntentsBitField } = require("discord.js");
+const { Configuration, OpenAIApi } = require("openai");
 
 const client = new Client({
   intents: [
@@ -10,5 +11,29 @@ const client = new Client({
 });
 
 client.on("ready", () => console.log("Bot is online!"));
+
+const configuration = new Configuration({
+  organization: "org-3Q0OIKZrldpIPqsW893nR2yj",
+  apiKey: process.env.OpenAI_API,
+});
+
+const openai = new OpenAIApi(configuration);
+
+client.on("messageCreate", async (msg) => {
+  await msg.channel.sendTyping();
+  let Log = [{ role: "system", content: "Chat bot!" }];
+
+  Log.push({
+    role: "user",
+    content: msg.content,
+  });
+
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: Log,
+  });
+
+  msg.reply(response.data.choices[0].message.content);
+});
 
 client.login(process.env.Discord_Token);
