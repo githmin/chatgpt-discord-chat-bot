1a6b56c56dc96859815f124670b38da08f77813c
Githmin Jayawardhana <Githmin21@gmail.com>
2023-03-27 11:29:35 +0530

    added discord config

---
+++ index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const { Client, IntentsBitField } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on("ready", () => console.log("Bot is online!"));
+
+client.login(process.env.Discord_Token);
